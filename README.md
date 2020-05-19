7d69c3bf38b498b26da505f41afce0fde5edd204
Leonardo Carvalho <leohoc@gmail.com>
2020-05-19 09:19:35 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# EPIDEMIC-TRANSMISSION-SIMULATION
+
+> Development in progress
---
+++ src/main/java/com/leohoc/ets/ETSApplication.java
@@ -0,0 +1,9 @@
+package com.leohoc.ets;
+
+public class ETSApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
